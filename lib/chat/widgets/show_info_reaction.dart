dfbb13fd9d6d1be0403083464c2a457046d9ba96
Trần Ngọc Tiến <94039133+ngoctienUIT@users.noreply.github.com>
2023-01-14 08:50:37 +0700

    update view reaction
    Cập nhật xíu

--- lib/chat/widgets/show_info_reaction.dart
+++ lib/chat/widgets/show_info_reaction.dart
@@ -1,8 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chat_app/chat/models/messages.dart';
+import 'package:chat_app/chat/models/user.dart' as myuser;
 import 'package:chat_app/chat/widgets/message_widget.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:chat_app/chat/models/user.dart' as myuser;
+
+import 'loading_image.dart';
 
 class ShowInfoReaction extends StatefulWidget {
   const ShowInfoReaction({Key? key, required this.messages}) : super(key: key);
@@ -35,9 +38,22 @@ class _ShowInfoReactionState extends State<ShowInfoReaction>
         borderRadius: BorderRadius.circular(15),
       ),
       margin: const EdgeInsets.fromLTRB(10, 0, 10, 20),
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
       child: Column(
         children: [
+          Row(
+            children: [
+              const Text(
+                "Cảm xúc",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              IconButton(
+                onPressed: () => Navigator.pop(context),
+                icon: const Icon(Icons.close),
+              )
+            ],
+          ),
           Expanded(
             child: ListView.builder(
               itemCount: widget.messages.reaction!.keys.length,
@@ -45,37 +61,54 @@ class _ShowInfoReactionState extends State<ShowInfoReaction>
                 if (tabController.index == 0 ||
                     widget.messages.reaction!.values.elementAt(index) ==
                         listReact[tabController.index - 1]) {
-                  return Row(
-                    children: [
-                      FutureBuilder<DocumentSnapshot>(
-                          future: FirebaseFirestore.instance
-                              .collection("users")
-                              .doc(widget.messages.reaction!.keys
-                                  .elementAt(index))
-                              .get(),
-                          builder: (context, snapshot) {
-                            if (snapshot.hasData) {
-                              myuser.User user = myuser.User.fromFirebase(
-                                  snapshot.requireData);
-                              return Text(
-                                user.name,
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold,
+                  return FutureBuilder<DocumentSnapshot>(
+                      future: FirebaseFirestore.instance
+                          .collection("users")
+                          .doc(widget.messages.reaction!.keys.elementAt(index))
+                          .get(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          myuser.User user =
+                              myuser.User.fromFirebase(snapshot.requireData);
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Row(
+                              children: [
+                                ClipOval(
+                                  child: CachedNetworkImage(
+                                    imageUrl: user.image!,
+                                    width: 40,
+                                    fit: BoxFit.cover,
+                                    placeholder: (context, url) => loadingImage(
+                                      width: 40,
+                                      height: 40,
+                                      radius: 90,
+                                    ),
+                                    errorWidget: (context, url, error) =>
+                                        const Icon(Icons.error),
+                                  ),
                                 ),
-                              );
-                            }
+                                const SizedBox(width: 10),
+                                Text(
+                                  user.name,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const Spacer(),
+                                Text(
+                                  react[widget.messages.reaction!.values
+                                      .elementAt(index)],
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          );
+                        }
 
-                            return Container();
-                          }),
-                      const Spacer(),
-                      Text(
-                        react[
-                            widget.messages.reaction!.values.elementAt(index)],
-                        style: const TextStyle(fontSize: 20),
-                      ),
-                    ],
-                  );
+                        return const SizedBox.shrink();
+                      });
                 }
                 return const SizedBox.shrink();
               },
